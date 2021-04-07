d03905a712e6c1d11f90bd2aadce4ab0213dc994
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-07 02:21:25 +0100

    Handle new user socket

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 const http = require('http');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const PORT = process.env.PORT || 5000;
@@ -17,9 +18,10 @@ app.use(cors());
 app.use(router);
 
 io.on('connection', (socket) => {
-  console.log('We have a new connection!');
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.join(user.room);
   });
   socket.on('disconnect', () => {
     console.log('User had left!');
